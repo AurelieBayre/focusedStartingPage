cf8d2468f1b0bf1ac7fa5d2c8fb3a0a09d3e478a
Aurelie Bayre <a.bayre@gmail.com>
2018-01-21 18:57:34 +0100

    storing works

--- myjs.js
+++ myjs.js
@@ -2,27 +2,12 @@
 //more specifically: get name, get goal, get goal date, get priorities.
 //and then, cache those in the browser!!!
 
-const goal = "Wild Code School";
+//Setting up localstorage.
+let myStorage = window.localStorage;
 
-//le jour-objectif:
-const countDownDay = new Date("Feb 23, 2018").getTime();
-//la donnée pour maintenant
-const now = new Date().getTime();
-//console.log(countDownDay, now);
+//myStorage.setItem("first test", "working!");
 
-//intervalle entre maintenant et l'objectif:
-const interval = countDownDay - now;
-
-//classons en jours:
-const remainingDays = Math.floor(interval/ (1000 * 60 * 60 * 24));
-
-//Afficher tout ça:
-const displayMessage = `${remainingDays} jours avant :`;
-
-document.getElementById("countDown").innerHTML = displayMessage;
-
-//const myPriorities = ["finir le cours PHP", "finir le cours MySql", "continuer les algos", "finir les exemples webdesign"];
-
+///OLD :
 const mesPrios = 
     [{name:"finir le cours php",
     url: "https://www.sololearn.com/Profile/6689025/PHP"},
@@ -34,6 +19,8 @@ const mesPrios =
     url: "https://github.com/AurelieBayre/integration"}
 ]
 
+
+//Display those priorities:
 function makeList(arr){
     for(let i = 0; i < arr.length; i ++){
       // console.log(i)
@@ -51,4 +38,86 @@ function makeList(arr){
 }
 makeList(mesPrios);
 
-document.getElementById("goal").innerHTML = goal + " !";
+///////////////////////////////////////////////////////////////
+//// NEW DISPPLAYING RULES (no hard coding: from user inputs)
+
+function remainingDays(dateStr){
+let goalDate = new Date(dateStr).getTime();
+let now = new Date().getTime();
+let interval = goalDate - now;
+let daysToGo = Math.floor(interval/ (1000 * 60 * 60 * 24));
+return daysToGo;
+}
+
+function display(str, target){
+     document.getElementById(target).innerHTML = str;
+}
+
+//forms
+function showSettingsForm(){
+    document.getElementById("getUserSettings").style.display = "block";
+    //ref: http://jsfiddle.net/rathoreahsan/vzmnJ/
+}
+
+let storageReady = myStorage.length > 0;
+
+
+function populateHTML(obj) {
+    
+    function alternateCountdown(str) {
+        if (str < 1) {
+            return `You've reached the deadline for:`
+        }
+        if (str === 1){
+            return `Only 1 day to go before:`
+        }
+        if (str > 1){
+            return `${str} days to go before:`
+        }
+    }
+    
+    let greetings = `Hello ${obj.name}!`;
+    let countdownMessage = alternateCountdown(obj.remaining);
+    let goalMessage = `${obj.goal}!`;
+
+    display(greetings, "name");
+    display(countdownMessage, "countDown")
+    display(goalMessage, "goal");
+}
+
+if (storageReady) {
+    //console.log("I can see storage is ready");
+   populateHTML(myStorage); 
+}
+//else{
+  //  console.log("so it's not quite working then...");
+//}
+document.getElementById("validateSettings").addEventListener("click", function(){
+    myStorage.clear();
+    
+    let settings = document.getElementById("generalSettings");
+    let userName = settings.elements["userName"].value;
+    let userGoal = settings.elements["userGoal"].value;
+    let userDeadline = settings.elements["userDeadline"].value;
+    let days = remainingDays(userDeadline);
+
+    let userData = {
+        name: userName,
+        goal: userGoal,
+        deadline: userDeadline,
+        remaining: days
+    }
+
+    //store this: 
+    if (!storageReady){
+        //console.log("I don't see storage is ready");
+        myStorage.setItem("name", userName);
+        myStorage.setItem("goal", userGoal);
+        myStorage.setItem("deadline", userDeadline);
+        myStorage.setItem("remaining", days);
+    }
+  
+    //set the template literals to be passed in the html:
+
+   populateHTML(userData); 
+   });
